e388e54a11b745f50daf55ef04e37a4b6cfd41d5
dennischerrez <dennis.cherrez@gmail.com>
2020-07-06 17:39:18 -0500

    agrgado de frase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> y refrescalo muchacho.
+          Edit <code>src/App.js</code> y refrescalo muchacho sin miedo.
         </p>
         <a
           className="App-link"

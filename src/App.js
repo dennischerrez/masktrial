7150126e9124140d67a2e6eb46e2fd444562706f
dennischerrez <40371220+dennischerrez@users.noreply.github.com>
2020-07-08 11:50:50 -0500

    cambie desde git hub
    porque puedo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> y fondo.
+          Edit <code>src/App.js</code> cambio desde github.
         </p>
         <a
           className="App-link"
